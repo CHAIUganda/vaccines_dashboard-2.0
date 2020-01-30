9ad8b4e716a87622bf7a37e65b3c8aa74c3420b0
Brian Ochan <bochan@outbox.co.ug>
2020-01-30 17:58:33 +0300

    Refactor: Backend routing
    - Application will route to the correct backend depending on whether
      its run on localhost or on the staging server.

---
+++ src/env_config.js
@@ -0,0 +1,13 @@
+const serverUrl = require("./config.json");
+// Determines the host and redirects traffic respective from production to test /staging
+function env_config() {
+  if (window.location.hostname === serverUrl.STAGING_URL) {
+    return serverUrl.STAGING_URL;
+  } else {
+    return serverUrl.LOCAL_URL;
+  }
+}
+let addr = env_config();
+
+console.log(addr);
+export default addr;
